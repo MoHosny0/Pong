f710959af5b75255d1768dcb3b137be9c7a08301
ybagoury <yalbagoury@gmail.com>
2022-03-23 14:40:55 +0100

    Vec2D tested and working

--- src/Vec2D.cpp
+++ src/Vec2D.cpp
@@ -3,24 +3,18 @@
 
 Vec2D Vec2D::operator+(Vec2D const &v)
 {
-    return Vec2(x + v.x, y + v.y);
+    return Vec2D(x + v.x, y + v.y);
 }
 
 Vec2D &Vec2D::operator+=(Vec2D const &v)
 {
     x += v.x;
     y += v.y;
+
     return *this;
 }
 
 Vec2D Vec2D::operator*(float k)
 {
-
     return Vec2D(x * k, y * k);
-}
-
-void Vec2D::setV(float x, float y)
-{
-    this.x = x;
-    this.y = y;
 }
--- src/Vec2D.h
+++ src/Vec2D.h
@@ -7,12 +7,10 @@ private:
     float x, y;
 
 public:
-    Vec2D() : x(0.0f), y(0.0f) {}           // default constructor
-    Vec2D(float x, float y) : x(x), y(y) {} // constructor with params
+    Vec2D() : x(0.0f), y(0.0f) {}
+    Vec2D(float x, float y) : x(x), y(y) {}
     Vec2D operator+(Vec2D const &v);
     Vec2D &operator+=(Vec2D const &v);
     Vec2D operator*(float k);
-    void setV(float x, float y);
-    Vec2D getV() const;
 };
 #endif
