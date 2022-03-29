34e4304b57df76a4f7a798fa87af5d4a486488d7
ybagoury <yalbagoury@gmail.com>
2022-03-29 09:38:00 +0200

    adding Ball

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,2 +1,21 @@
 #include "Ball.h"
 
+Ball::Ball(const Vec2D &InitialPosition, const float BallRadius)
+{
+    position = InitialPosition;
+    radius = BallRadius;
+    velocity(0, 0);
+}
+const Vec2D &Ball : GetPosition() const { return position; }
+const Vec2D &Ball::GetVelocity() const { return velocity; }
+const float &Ball::GetRadius() const { return radius; }
+void Ball::SetPosition(const Vec2D &NewPosition)
+{
+    position = NewPosition;
+}
+void Ball::SetVelocity(const Vec2D &NewVelocity)
+{
+    velocity = NewVelocity;
+}
+}
+;
--- src/Ball.h
+++ src/Ball.h
@@ -1,11 +1,20 @@
+#include "Vec2D.h"
 #ifndef BALL_H
 #define BALL_H
 
 class Ball
 {
 private:
+    Vec2D position;
+    Vec2D velocity;
+    float radius;
 
-public: 
-
+public:
+    Ball(const Vec2D &pos, const float radius);
+    const Vec2D &GetPosition() const;
+    const Vec2D &GetVelocity() const;
+    const float &GetRadius() const;
+    void SetPosition(const Vec2D &NewPosition);
+    void SetVelocity(const Vec2D &NewVelocity);
 };
 #endif
