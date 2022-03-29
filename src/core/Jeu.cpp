226aa2286605f16a8f9f2d8c674dc72e1cd87921
mohosny <mh116407@gmail.com>
2022-03-29 12:17:35 +0200

    addngg mod jeu

--- src/core/Jeu.cpp
+++ src/core/Jeu.cpp
@@ -1,1 +1,12 @@
 #include "Jeu.h"
+
+Jeu::Jeu()
+{
+}
+
+Terrain &Jeu::getTerrain() { return ter; }
+Paddle &Jeu::getPaddle() { return p; }
+
+const Terrain &Jeu::getConstTerrain() const { return ter; }
+const Ball &Jeu::getConstBall() const { return b; }
+const Paddle &Jeu::getConstPaddle() const { return p; }
--- src/core/Jeu.h
+++ src/core/Jeu.h
@@ -11,9 +11,18 @@ private:
     Terrain ter;
     Ball b;
     Paddle p;
+    int player1_score, player2_score;
+    Paddle player1, player2;
 
 public:
     Jeu();
+
+    Terrain &getTerrain();
+    Paddle &getPaddle();
+
+    const Terrain &getConstTerrain() const;
+    const Ball &getConstBall() const;
+    const Paddle &getConstPaddle() const;
 };
 
 #endif
